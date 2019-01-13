5879fe06223cafb7483c1bcfb1f9d04ee216582b
bruce-he-62 <bruce.he.62@gmail.com>
2019-01-12 21:57:58 -0500

    Modifications to storage scheme.

--- storage.js
+++ storage.js
@@ -1,35 +1,49 @@
-class dailyQuestions {
-    constructor(questions, number_questions)
+class weekly_responses {
+    constructor(number)
     {
-        this.questions = questions;
-        this.answers = Array(number_questions).fill(Array(7).fill(""));
-    }
-
-    set_question() {
-        
+        this.answers = Array(number).fill([" "," "," "," "," "," "," "]);
     }
 
     addResponse(question_number, day, response) {
-        answers[question_number][day] = response;
+        answers[question_number][day-1] = response;
     }
 
     returnResponse(question_number, day) {
-        return(answers[question_number][day]);
+        return(answers[question_number][day-1]);
+    }
+
+    returnResponse(question_number)
+    {
+        return(answers[question_number]);
     }
 }
 
-class extendedQuestions {
-    constructor(questions, number_questions)
+class response_record {
+    constructor(number)
     {
-        this.questions = questions;
-        this.response_record = Array(0).fill(new dailyQuestions(questions, number_questions));
-        this.weeks = 0;
+        this.total_weeks = 0;
+        this.response_record = Array(0).fill(new weekly_responses(number));
     }
 
-    add_week(week_questions)
+    add_week(responses_of_the_week)
     {
-        response_record.concat(week_questions);
-        weeks = weeks+1;
+        response_record.concat(responses_of_the_week);
+        total_weeks = total_weeks + 1;
+    }
+
+    return_week(week_number) 
+    {
+        return(response_record[week_number]);
+    }
+
+    return_all_question_responses(question_number)
+    {
+        var total_response = Array(0);
+        for(i = 0; i < total_weeks; i++)
+        {
+            total_response.concat(response_record[i].returnresponse(question_number));
+        }
+        return total_response;
     }
 
 }
